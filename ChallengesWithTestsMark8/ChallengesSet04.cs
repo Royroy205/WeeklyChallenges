5e7c53f498519fd478bb72e6c181786135d8ef47
Royroy205 <leroyjr2344@live.com>
2023-09-06 14:09:43 -0500

    Weekly 4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +7,156 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+                else
+                {
+                    sum -= num;
+                }
+            }
+            return sum;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            int minLength = int.MaxValue;
+
+            if (str1 != null && str1.Length < minLength)
+            {
+                minLength = str1.Length;
+            }
+
+            if (str2 != null && str2.Length < minLength)
+            {
+                minLength = str2.Length;
+            }
+
+            if (str3 != null && str3.Length < minLength)
+            {
+                minLength = str3.Length;
+            }
+
+            if (str4 != null && str4.Length < minLength)
+            {
+                minLength = str4.Length;
+            }
+
+            return minLength;
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int smallest = int.MaxValue;
+
+            if (number1 < smallest)
+            {
+                smallest = number1;
+            }
+
+            if (number2 < smallest)
+            {
+                smallest = number2;
+            }
+
+            if (number3 < smallest)
+            {
+                smallest = number3;
+            }
+
+            if (number4 < smallest)
+            {
+                smallest = number4;
+            }
+
+            return smallest;
         }
+    
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            if ((sideLength1 + sideLength2 <= sideLength3) || (sideLength1 + sideLength3 <= sideLength2) || (sideLength2 + sideLength3 <= sideLength1))
+            {
+                return false;
+            }
+            return true;
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            return input != null && (double.TryParse(input, out double dInput));
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            if (objs == null || objs.Length == 0)
+            {
+                
+                return false; 
+            }
+
+            int nullCount = 0;
+            int notNullCount = 0;
+
+            foreach (object obj in objs)
+            {
+                if (obj == null)
+                {
+                    nullCount++;
+                }
+                else
+                {
+                    notNullCount++;
+                }
+            }
+
+            return nullCount > notNullCount;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || !numbers.Any())
+            {
+                return 0;
+            }
+
+            double evenSum = 0;
+            int evenCount = 0;
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evenSum += num;
+                    evenCount++;
+                }
+            }
+            return evenCount > 0 ? evenSum / evenCount : 0;
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number), "Factorial is not defined for negative numbers.");
+            }
+
+            int result = 1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
+
+            return result;
         }
     }
 }
